280aa5ab240902a13cf8b66628f53174783dd0d9
Max Ball <max.ball3388@gmail.com>
2022-08-07 01:22:20 -0600

    completed

--- app/Controllers/WeathersController.js
+++ app/Controllers/WeathersController.js
@@ -3,21 +3,41 @@ import { weathersService } from "../Services/WeathersService.js";
 import { Pop } from "../Utils/Pop.js";
 
 
-function _draw(){
-  let weather = ProxyState.weather.temp
-    let c = weather - 273
-    let f = c * (9/5) + 32
-    let fahrenheit = Math.floor(f)
-  document.getElementById('weather').innerHTML = `
-      <h4>${fahrenheit}</h4>
-      <h6>${ProxyState.weather.name}</h6>
-    
-  `
-}
+
+
+
+
+
+
 export class WeathersController{
   constructor(){
-    ProxyState.on('weather', _draw)
+    ProxyState.on('weather', this.draw)
     this.getWeather()
+  }
+
+  changeTemp(){
+    console.log('getting here?');
+    let weather = ProxyState.weather.temp
+    let c = Math.floor(weather - 273)
+    let f = Math.floor(c * (9/5) + 32)
+  
+      console.log('how about here?');
+      document.getElementById('weather').innerHTML = `
+      <h4 class="selectable no-select" onclick="app.weathersController.draw()">${c}° C</h4>
+      <h6>${ProxyState.weather.name}</h6>
+      `
+    }
+  
+
+  draw(){
+    let weather = ProxyState.weather.temp
+      let c = Math.floor(weather - 273)
+      let f = Math.floor(c * (9/5) + 32)
+      
+        document.getElementById('weather').innerHTML = `
+          <h4 class="selectable no-select" onclick="app.weathersController.changeTemp()">${f}° F</h4>
+          <h6>${ProxyState.weather.name}</h6>
+        `
   }
 
   async getWeather(){
